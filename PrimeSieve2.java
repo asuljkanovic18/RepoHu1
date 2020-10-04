9e3f4d4879341786b7a3cd33a22315e36087c8b5
unknown <anelsuljkanovic04@gmail.com>
2020-10-04 13:43:07 +0200

    interface erstellt

---
+++ PrimeSieve2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primesieve2;
+
+/**
+ *
+ * @author anels
+ */
+interface PrimeSieve2 {
+        public void isPrime(int p);
+        
+        public void PrintPrimes(int max);
+}
